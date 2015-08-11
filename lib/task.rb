2248f410c38c939adbdf3ba2faaf6537fd02854c
Trevor <scherling.trevor@gmail.com>
2015-08-10 23:47:13 -0700

    Add spec and code to folder.

---
+++ lib/task.rb
@@ -0,0 +1,19 @@
+class Task
+  @@all_task = []
+
+  define_method(:initialize) do |description|
+    @description = description
+  end
+
+  define_method(:description) do
+    @description
+  end
+
+  define_singleton_method(:all) do
+    @@all_task
+  end
+
+  define_method(:save) do
+    @@all_task.push(self)
+  end
+end
